26c4b5657066afa60f83e9fe5d604a46ccbf88d7
Ankur Pandey <ankurpandey.4683@gmail.com>
2023-09-18 18:10:43 +0530

    RGB light in NeoPixel LED

--- Neopixel_LED/main.py
+++ Neopixel_LED/main.py
@@ -12,9 +12,27 @@ np = neopixel.NeoPixel(machine.Pin(48),1)
 
 while True:
     try:
+        np[0]=[255,0,0]
+        np.write()
+        time.sleep(0.1)
+        np[0]=[0,255,0]
+        np.write()
+        time.sleep(0.1)
+        np[0]=[0,0,255]
+        np.write()
+        time.sleep(0.1)
+        np[0]=[255,255,0]
+        np.write()
+        time.sleep(0.1)
+        np[0]=[128,0,128]
+        np.write()
+        time.sleep(0.1)
         np[0]=[255,0,255]
         np.write()
-        time.sleep(0.2)
+        time.sleep(0.1)
+        np[0]=[255,192,203]
+        np.write()
+        time.sleep(0.1)
     except KeyboardInterrupt:
         print("Exit")
         break
