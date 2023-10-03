c944e8f8e1fddbcc806e758b169a4a5eedd049ca
Ankur Pandey <ankurpandey.4683@gmail.com>
2023-10-03 18:25:38 +0530

    ESP server codes

--- Controlling_Neopixels_with_interrupts/main.py
+++ Controlling_Neopixels_with_interrupts/main.py
@@ -20,8 +20,10 @@ def toggle_led(p):
     global value
     if value == 0:
         value = 255
+        print("Neopixel is on")
     elif value == 255:
         value = 0
+        print("Neopixel is off")
     np[0] = (value,0,0)
     np.write()
 
